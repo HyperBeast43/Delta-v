3de0cfacf65ec68ea40d77ab3fa3b0ee5296ba7c
Hyper <45052445+HyperBeast43@users.noreply.github.com>
2026-05-17 19:38:48 -0500

    !,?,…!,…?,…
    Signed-off-by: Hyper <45052445+HyperBeast43@users.noreply.github.com>

--- Content.Server/Chat/Managers/ChatSanitizationManager.cs
+++ Content.Server/Chat/Managers/ChatSanitizationManager.cs
@@ -92,6 +92,11 @@ public sealed class ChatSanitizationManager : IChatSanitizationManager
         Entry("[':", "chatsan-tearfully-smiles"),
         Entry("('=", "chatsan-tearfully-smiles"),
         Entry("['=", "chatsan-tearfully-smiles"),
+        Entry("!", "chatsan-surprised"), //DeltaV
+        Entry("?", "chatsan-confused"), //DeltaV
+        Entry("…!", "chatsan-wide-eyed"), //DeltaV (note that `...` doesn't work as that turns into a radio message)
+        Entry("…?", "chatsan-uncertain"), //DeltaV (who uses `…` instead of `...`? me! i do that. compose key go brr)
+        Entry("…", "chatsan-sighs"), //DeltaV 
     ];
 
     [Dependency] private readonly IConfigurationManager _configurationManager = default!;
